0212e22f31616b9f3e44023e76686ce1347bcacb
manthanparekh <manthan.parekh@outlook.com>
2023-09-04 21:08:18 -0400

    mod change

---
+++ index.js
@@ -0,0 +1,21 @@
+//TODO: function in javascript to change location in html 
+// TODO: links https://github.com/manthanparekh
+// TODO: links https://www.linkedin.com/in/manthanparekh01
+// TODO: links https://www.instagram.com/manthanparekh/ 
+// TODO: links https://www.facebook.com/manthan.parekh.35   
+
+function githubLink() {
+
+}
+
+function linkedinLink() {
+
+}
+
+function instagramLink() {
+
+}
+
+function facebookLink() {
+    
+}
